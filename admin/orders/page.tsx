cc9b5045ee9ed79f76e979eed672966190b91a83
raileats <raileats@gmail.com>
2025-09-05 04:21:30 +0530

    Delete admin directory

--- admin/orders/page.tsx
+++
@@ -1,3 +0,0 @@
-"use client";
-import React from "react";
-export default function Stub(){return <div className="p-4">Placeholder (auto-generated) for raileats-admin-sep-main/admin/orders/page.tsx</div>;}
